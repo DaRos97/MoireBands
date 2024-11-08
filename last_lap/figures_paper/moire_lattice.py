913e2af23affbe6931da8009d7df71c9d0a2df33
DaRos97 <dario.rossim@gmail.com>
2024-11-08 17:52:58 +0100

    Orbital content figure finshed

--- last_lap/figures_paper/moire_lattice.py
+++ last_lap/figures_paper/moire_lattice.py
@@ -9,9 +9,9 @@ x = np.array([1,0])
 y = np.array([0,1])
 
 a_WSe2 = 1.1
-a_WS2 = 3.18
 a_WS2 = 1.
 a_M = 1/np.sqrt(1/a_WSe2**2+1/a_WS2**2-2/a_WSe2/a_WS2)
+
 print("moire length: ",a_M)
 
 nx = ny = int(a_M/a_WSe2)+10
@@ -26,17 +26,19 @@ for i in range(-nx,nx):
         l_WS2[i+nx,j+ny,0] = (i*a1+j*a2+x/2-y/2/np.sqrt(3))*a_WS2
         l_WS2[i+nx,j+ny,1] = (i*a1+j*a2+x/2+y/2/np.sqrt(3))*a_WS2
 
-fig,axs = plt.subplots(1,2,figsize=(12,5))
-for parallel in [True,False]:
-    ax = axs[0] if parallel else axs[1]
+fig= plt.figure(figsize=(20,10))
+shapes = [['o','^'],['^','o']]
+titles = ['Parallel','Anti-parallel']
+for i in range(2):
+    ax = fig.add_subplot(2,2,2*i+1)
     ax.set_aspect('equal')
     #
-    shape1 = ['o','+']
-    shape2 = shape1 if parallel else ['+','o']
+    shape1 = ['o','^']
+    shape2 = shapes[i]
     for y in range(-ny,ny):
         for n in range(2):      #sublattice index
-            ax.scatter(l_WSe2[:,y+ny,n,0],l_WSe2[:,y+ny,n,1],color='b',marker=shape1[n])
-            ax.scatter(l_WS2[:,y+ny,n,0],l_WS2[:,y+ny,n,1],color='r',marker=shape2[n])
+            ax.scatter(l_WSe2[:,y+ny,n,0],l_WSe2[:,y+ny,n,1],color='b',marker=shape1[n],lw=1,facecolor='none')
+            ax.scatter(l_WS2[:,y+ny,n,0],l_WS2[:,y+ny,n,1],color='r',marker=shape2[n],lw=1,facecolor='none')
         for x in range(-nx,nx):
             x1,y1 = (x*a1+y*a2)*a_WSe2
             x2,y2 = (x*a1+y*a2)*a_WS2
@@ -46,16 +48,23 @@ for parallel in [True,False]:
             hexagon2 = patches.RegularPolygon((x2,y2), numVertices=6, radius=r2, orientation=0, edgecolor='r', facecolor='none', zorder=0, lw=0.2)
             ax.add_patch(hexagon1)
             ax.add_patch(hexagon2)
-
-    ax.arrow(0,0,a1[0]*a_M,a1[1]*a_M,color='k',head_width=0.,zorder=0,lw=0.5)
-    ax.arrow(0,0,a2[0]*a_M,a2[1]*a_M,color='k',head_width=0.,zorder=0,lw=0.5)
-    ax.arrow(a1[0]*a_M,a1[1]*a_M,a2[0]*a_M,a2[1]*a_M,color='k',head_width=0.,zorder=0,lw=0.5)
-    ax.arrow(a2[0]*a_M,a2[1]*a_M,a1[0]*a_M,a1[1]*a_M,color='k',head_width=0.,zorder=0,lw=0.5)
+    #UC
+    if 0:
+        ax.arrow(0,0,a1[0]*a_M,a1[1]*a_M,color='k',head_width=0.,zorder=0,lw=0.5)
+        ax.arrow(0,0,a2[0]*a_M,a2[1]*a_M,color='k',head_width=0.,zorder=0,lw=0.5)
+        ax.arrow(a1[0]*a_M,a1[1]*a_M,a2[0]*a_M,a2[1]*a_M,color='k',head_width=0.,zorder=0,lw=0.5)
+        ax.arrow(a2[0]*a_M,a2[1]*a_M,a1[0]*a_M,a1[1]*a_M,color='k',head_width=0.,zorder=0,lw=0.5)
+    else:
+        hexagon = patches.RegularPolygon((0,0), numVertices=6, radius=a_M/np.sqrt(3), orientation=0, edgecolor='k', facecolor='none', zorder=2, lw=2)
+        ax.add_patch(hexagon)
+    #
     ax.axis('off')
 
-    ax.set_xlim(-1.2*a_M/2,1.1*a_M)
-    ax.set_ylim(-a_WSe2*1.5,1.2*a_M/2*np.sqrt(3))
-    title = 'Parallel' if parallel else 'Anti-parallel'
-    ax.set_title(title,size=20)
+#    ax.set_xlim(-1.2*a_M/2,1.1*a_M)
+#    ax.set_ylim(-a_WSe2*1.5,1.2*a_M/2*np.sqrt(3))
+    ax.set_xlim(-a_M,a_M)
+    ax.set_ylim(-a_M,a_M)
+#
+    ax.set_title(titles[i],size=20)
 fig.tight_layout()
 plt.show()
