Adjusted bug on nans and float precision dirname

Code/bilayer_v2.0/edc.py
@@ -32,7 +32,6 @@ from pathlib import Path
 
 machine = cfs.get_machine(os.getcwd())
 n_chunks = 128
-computeGap = True
 
 """ Parameters and options """
 if len(sys.argv)!=3:
@@ -48,6 +47,7 @@ if machine=='maf':
 if ind<0 or ind>=n_chunks:
     raise ValueError("Index out of range: ",ind)
 disp = machine=='loc'
+computeGap = BZpoint=='K'
 
 """ Fixed parametetrs """
 theta_deviation = 0      #Change here for \pm 0.3 degrees
@@ -104,7 +104,7 @@ for pars in parameters_chunk:
             print("Vg: %.3f\tphiG: %.1f\tw1p: %.3f\t w1d: %.3f"%(Vg,phiG/np.pi*180,w1p,w1d))
     elif BZpoint=='K':
         Vk, phiK = pars
-        if disp:
+        if 0:#disp:
             Vk = 0.0077
             phiK = 106/180*np.pi
             print("Vk: %.4f\tphiK: %.1f"%(Vk,phiK/np.pi*180))
@@ -130,7 +130,7 @@ for pars in parameters_chunk:
             resultsGap.append((*pars,gap))
         results.append((*pars,*positions))
     else:
-        results.append((*pars,np.full(2 if BZpoint=='K' else 3,np.nan)))
+        results.append((*pars,*np.full(2 if BZpoint=='K' else 3,np.nan)))
         if computeGap:
             resultsGap.append((*pars,np.nan))
 
@@ -142,7 +142,7 @@ df = pd.DataFrame(
 dirname = cfs.getFilename(
     ('edc'+BZpoint,theta_deviation,nShells,spreadE,*argsFn),
     dirname=utils.get_home_dn(machine)+"Data/",
-    floatPrecision=3,
+    floatPrecision=4,
 ) + '_' + listFn + '/'
 if not Path(dirname).is_dir():
     os.system("mkdir "+dirname)
@@ -163,7 +163,7 @@ if computeGap:
     dirnameGap = cfs.getFilename(
         ('edcGap'+BZpoint,theta_deviation,nShells,spreadE,*argsFn),
         dirname=utils.get_home_dn(machine)+"Data/",
-        floatPrecision=3,
+        floatPrecision=4,
     ) + '_' + listFn + '/'
     if not Path(dirnameGap).is_dir():
         os.system("mkdir "+dirnameGap)

Code/bilayer_v2.0/utils.py
@@ -14,10 +14,10 @@ import lmfit      #for fitting weights in EDC
 def get_parameters(chunk_id,BZpoint,n_chunks=128):
     """ Get chunks of parameters to compute. """
     if BZpoint=='G':
-        listVg = np.linspace(0.001,0.025,25)            # Moirè potential amplitude -> every 1 meV
-        listPhi = np.linspace(160,180,21) /180*np.pi    # Moiré potential phase -> every 1 °
-        listW1p = np.linspace(-2.000, 2.000,41)         # Interlayer coupling p -> every 10 meV in fine grid, every 50 meV in large grid
-        listW1d = np.linspace(-2.000, 2.000,41)         # Interlayer coupling d -> every 10 meV in fine grid, every 50 meV in large grid
+        listVg = np.linspace(0.010,0.020,51)            # Moirè potential amplitude -> every 1 meV
+        listPhi = np.linspace(0,359,360) /180*np.pi    # Moiré potential phase -> every 1 °
+        listW1p = np.linspace(-1.580,-1.530,11)         # Interlayer coupling p -> every 10 meV in fine grid, every 50 meV in large grid
+        listW1d = np.linspace( 1.120, 1.170,11)         # Interlayer coupling d -> every 10 meV in fine grid, every 50 meV in large grid
         filename = cfs.getFilename(
             (
             listVg[0],listVg[-1],len(listVg),int(listPhi[0]/np.pi*180),int(listPhi[-1]/np.pi*180),len(listPhi),
@@ -28,8 +28,8 @@ def get_parameters(chunk_id,BZpoint,n_chunks=128):
         grid = product(listVg, listPhi, listW1p, listW1d)
         total_jobs = len(listVg)*len(listPhi)*len(listW1p)*len(listW1d)
     elif BZpoint=='K':
-        listVk = np.linspace(0.001,0.040,128)     # Considered values of moirè potential -> every 1 meV
-        listPhiK = np.linspace(0,359,1) /180*np.pi
+        listVk = np.linspace(0.001,0.040,196)     # Considered values of moirè potential -> every 1 meV
+        listPhiK = np.linspace(0,359,360) /180*np.pi
         filename = cfs.getFilename(
             ( listVk[0],listVk[-1],len(listVk),int(listPhiK[0]/np.pi*180),int(listPhiK[-1]/np.pi*180),len(listPhiK) )
         )